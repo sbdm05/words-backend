71f5d6c4df89e3046cf7f4acf44fcd864557ca44
Sonia-Baibou <s.baibou@ohmycode.io>
2023-05-11 11:20:34 +0200

    add controllers

--- controllers/words.js
+++ controllers/words.js
@@ -1,11 +1,25 @@
 const { request } = require('express');
 require('dotenv').config();
+const Word = require('../models/Word');
 
 const getWords = async (req, res)=>{
-      const { body } = req;
-      console.log(body, 'body');
-      res.send('test')
-}
+  const { body } = req;
+  console.log(body, 'body');
+  //res.send('test')
+
+
+
+    try {
+      // utiliser la méthode Model.find({})
+      const words = await Word.find({});
+
+      return res.json({ success: true, words: words });
+    } catch (error) {
+      console.log('erreur dans get');
+      res.status(500).json({ msg: error });
+    }
+  }
+
 
 
 module.exports = {
---
+++ models/Word.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+// on crée la structure de Todo
+const WordSchema = new mongoose.Schema({
+  // key/value pair
+  french: {
+    type: String,
+  },
+  meaning: {
+    type: String,
+  },
+  english: {
+    type: String,
+  },
+  english__1: {
+    type: String,
+  },
+  syn: {
+    type: String,
+  },
+  etym: {
+    type: String,
+  },
+  english: {
+    type: Number,
+  },
+  mobile_share: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('Word', WordSchema);
